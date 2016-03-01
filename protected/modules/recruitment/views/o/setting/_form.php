1655d220cd4f141a9519e4641f140803a7c59070
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-03-01 14:03:04 +0700

    recruitment setting: add crud source

---
+++ protected/modules/recruitment/views/o/setting/_form.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * Recruitment Settings (recruitment-setting)
+ * @var $this SettingController
+ * @var $model RecruitmentSetting
+ * @var $form CActiveForm
+ *
+ * @author Putra Sudaryanto <putra.sudaryanto@gmail.com>
+ * @copyright Copyright (c) 2016 Ommu Platform (ommu.co)
+ * @created date 1 March 2016, 13:53 WIB
+ * @link http://company.ommu.co
+ * @contect (+62)856-299-4114
+ *
+ */
+?>
+
+<?php $form=$this->beginWidget('application.components.system.OActiveForm', array(
+	'id'=>'recruitment-setting-form',
+	'enableAjaxValidation'=>true,
+	//'htmlOptions' => array('enctype' => 'multipart/form-data')
+)); ?>
+
+<?php //begin.Messages ?>
+<div id="ajax-message">
+	<?php echo $form->errorSummary($model); ?>
+</div>
+<?php //begin.Messages ?>
+
+<fieldset>
+
+	<div class="clearfix">
+		<?php echo $form->labelEx($model,'license'); ?>
+		<div class="desc">
+			<?php echo $form->textField($model,'license',array('size'=>32,'maxlength'=>32)); ?>
+			<?php echo $form->error($model,'license'); ?>
+			<?php /*<div class="small-px silent"></div>*/?>
+		</div>
+	</div>
+
+	<div class="clearfix publish">
+		<?php echo $form->labelEx($model,'permission'); ?>
+		<div class="desc">
+			<?php echo $form->checkBox($model,'permission'); ?>
+			<?php echo $form->labelEx($model,'permission'); ?>
+			<?php echo $form->error($model,'permission'); ?>
+			<?php /*<div class="small-px silent"></div>*/?>
+		</div>
+	</div>
+
+	<div class="clearfix">
+		<?php echo $form->labelEx($model,'meta_keyword'); ?>
+		<div class="desc">
+			<?php echo $form->textArea($model,'meta_keyword',array('rows'=>6, 'cols'=>50)); ?>
+			<?php echo $form->error($model,'meta_keyword'); ?>
+			<?php /*<div class="small-px silent"></div>*/?>
+		</div>
+	</div>
+
+	<div class="clearfix">
+		<?php echo $form->labelEx($model,'meta_description'); ?>
+		<div class="desc">
+			<?php echo $form->textArea($model,'meta_description',array('rows'=>6, 'cols'=>50)); ?>
+			<?php echo $form->error($model,'meta_description'); ?>
+			<?php /*<div class="small-px silent"></div>*/?>
+		</div>
+	</div>
+
+	<div class="clearfix">
+		<?php echo $form->labelEx($model,'modified_date'); ?>
+		<div class="desc">
+			<?php echo $form->textField($model,'modified_date'); ?>
+			<?php echo $form->error($model,'modified_date'); ?>
+			<?php /*<div class="small-px silent"></div>*/?>
+		</div>
+	</div>
+
+	<div class="clearfix">
+		<?php echo $form->labelEx($model,'modified_id'); ?>
+		<div class="desc">
+			<?php echo $form->textField($model,'modified_id',array('size'=>11,'maxlength'=>11)); ?>
+			<?php echo $form->error($model,'modified_id'); ?>
+			<?php /*<div class="small-px silent"></div>*/?>
+		</div>
+	</div>
+
+	<div class="submit clearfix">
+		<label>&nbsp;</label>
+		<div class="desc">
+			<?php echo CHtml::submitButton($model->isNewRecord ? Phrase::trans(1,0) : Phrase::trans(2,0), array('onclick' => 'setEnableSave()')); ?>
+		</div>
+	</div>
+
+</fieldset>
+<?php /*
+<div class="dialog-content">
+</div>
+<div class="dialog-submit">
+	<?php echo CHtml::submitButton($model->isNewRecord ? Phrase::trans(1,0) : Phrase::trans(2,0) ,array('onclick' => 'setEnableSave()')); ?>
+	<?php echo CHtml::button(Phrase::trans(4,0), array('id'=>'closed')); ?>
+</div>
+*/?>
+<?php $this->endWidget(); ?>
+
+
---
+++ protected/modules/recruitment/views/o/setting/front_index.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Recruitment Settings (recruitment-setting)
+ * @var $this SettingController
+ * @var $model RecruitmentSetting
+ * @var $dataProvider CActiveDataProvider
+ *
+ * @author Putra Sudaryanto <putra.sudaryanto@gmail.com>
+ * @copyright Copyright (c) 2016 Ommu Platform (ommu.co)
+ * @created date 1 March 2016, 13:53 WIB
+ * @link http://company.ommu.co
+ * @contect (+62)856-299-4114
+ *
+ */
+
+	$this->breadcrumbs=array(
+		'Recruitment Settings',
+	);
+?>
+
+<?php $this->widget('application.components.system.FListView', array(
+	'dataProvider'=>$dataProvider,
+	'itemView'=>'_view',
+	'pager' => array(
+		'header' => '',
+	), 
+	'summaryText' => '',
+	'itemsCssClass' => 'items clearfix',
+	'pagerCssClass'=>'pager clearfix',
+)); ?>
