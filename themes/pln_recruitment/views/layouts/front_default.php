0aea6f1fbce7530759fbab1f61a2a78bc83fe42e
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-03-02 17:16:18 +0700

    recruitment: commit

--- themes/pln_recruitment/views/layouts/front_default.php
+++ themes/pln_recruitment/views/layouts/front_default.php
@@ -32,21 +32,8 @@
 	<h1><?php echo CHtml::encode($this->pageTitle); ?></h1>
 <?php }?>
 
-<div id="<?php echo $class;?>" class="box-wrap <?php echo $this->adsSidebar == true ? 'ads-on' : '';?>">	
-	<?php if($this->adsSidebar == true) {?>
-		<div class="content <?php echo $action;?>">
-			<div class="boxed clearfix">
-				<?php echo $content;?>
-			</div>
-		</div>
-		<div class="sidebar">
-			<div class="boxed clearfix">
-				aaa
-			</div>
-		</div>
-	<?php } else {
-		echo $content;
-	}?>
+<div id="<?php echo $class;?>" class="box-wrap">
+	<?php echo $content;?>
 </div>
 
 <?php $this->endContent(); ?>
